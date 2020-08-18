0e101c2e537d0aee3238494f9de44deff2df2446
Alexander Dahlin <alexander.dahlins@gmail.com>
2020-08-19 01:25:15 +0200

    Update TSharedPtr.h

--- Testbench/TSharedPtr.h
+++ Testbench/TSharedPtr.h
@@ -54,12 +54,7 @@ public:
 	template<typename TOther>
 	friend class TPtrBase;
 
-	FORCEINLINE T* Get() noexcept
-	{
-		return Ptr;
-	}
-
-	FORCEINLINE const T* Get() const noexcept
+	FORCEINLINE T* Get() const noexcept
 	{
 		return Ptr;
 	}
@@ -313,6 +308,11 @@ public:
 	{
 	}
 
+	FORCEINLINE TSharedPtr(std::nullptr_t) noexcept
+		: TBase()
+	{
+	}
+
 	FORCEINLINE explicit TSharedPtr(T* InPtr) noexcept
 		: TBase()
 	{
@@ -356,7 +356,7 @@ public:
 	}
 
 	template<typename TOther>
-	FORCEINLINE explicit TSharedPtr(TUniquePtr<TOther>&& Other) noexcept
+	FORCEINLINE TSharedPtr(TUniquePtr<TOther>&& Other) noexcept
 		: TBase()
 	{
 		static_assert(std::is_convertible<TOther, T>());
@@ -426,7 +426,7 @@ public:
 
 		if (this != std::addressof(Other))
 		{
-			Reset(); 
+			Reset();
 			TBase::InternalMove<TOther>(Move(Other));
 		}
 
@@ -434,7 +434,7 @@ public:
 	}
 
 	FORCEINLINE TSharedPtr& operator=(T* InPtr) noexcept
-	{	
+	{
 		if (Ptr != InPtr)
 		{
 			Reset();
