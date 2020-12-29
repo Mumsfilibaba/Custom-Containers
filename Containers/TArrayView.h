6e5649ac5c975f90813826eefbffc20c47f25fd9
Alexander Dahlin <alexander@dahlins.nu>
2020-12-29 01:04:51 +0100

    Update TArrayView.h

--- Containers/TArrayView.h
+++ Containers/TArrayView.h
@@ -1,3 +1,32 @@
 #pragma once
+#if 0
+#include "TUtilities.h"
 
+/*
+ * TArrayView
+ */
 
+template<typename T>
+class TArrayView
+{
+public:
+	template<typename TArrayType>
+	FORCEINLINE TArrayView(const TArrayType& Array)
+		: Begin(Array.Data())
+		, End(Array.Data() + Array.Size())
+		, Size(Array.Size())
+	{
+	}
+	
+	template<const UInt32 N>
+	FORCEINLINE TArrayView(T(&Array)[N])
+	{
+	}
+	
+private:
+	T* Begin;
+	T* End;
+	UInt32 Size;
+};
+
+#endif
